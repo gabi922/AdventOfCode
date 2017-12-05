85d0588494d2979250fd45dae8187d9838e6d82d
Gabi Raducu <gabi.raducu25@gmail.com>
2017-12-05 21:03:08 +0200

    day6: create day6 skeleton

---
+++ AdventOfCode2017.Test/Day6/Day6SolverTest.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode2017.Day6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2017.Test.Day6
+{
+    [TestClass]
+    public class Day6SolverTest
+    {
+        Day6Solver day6Solver;
+
+        [TestInitialize]
+        public void Start()
+        {
+            day6Solver = new Day6Solver();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var inputFile = "./Day6/input1.txt";
+            var result = day6Solver.Solve(Utils.ReadArray(inputFile));
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day6/Day6Solver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2017.Day6
+{
+    public class Day6Solver
+    {
+        public int Solve(List<int> input)
+        {
+            return input.Count;
+        }
+    }
+}
