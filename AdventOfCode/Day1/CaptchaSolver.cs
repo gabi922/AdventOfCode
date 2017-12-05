82f89d83064beea45f1c9af7b1db5b2de7544058
Gabi Raducu <gabi.raducu25@gmail.com>
2017-12-05 12:37:41 +0200

    Add project files.

---
+++ AdventOfCode/Day1/CaptchaSolver.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2017.Day1
+{
+    public class CaptchaSolver
+    {
+        public int GetCaptchaSum(string captcha, int step)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < captcha.Length; i++)
+            {
+                if (captcha[i] == captcha[(i + step) % captcha.Length])
+                {
+                    sum += captcha[i] - '0';
+                }
+            }
+
+            return sum;
+        }
+    }
+}
