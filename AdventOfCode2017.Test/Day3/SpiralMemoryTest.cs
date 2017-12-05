1e7eccd2b17f7be50f67def04bc3bec6c2bc57e3
Gabi Raducu <gabi.raducu25@gmail.com>
2017-12-05 16:27:48 +0200

    day3: part1

---
+++ AdventOfCode2017.Test/Day3/SpiralMemoryTest.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode2017.Day3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2017.Test.Day3
+{
+    [TestClass]
+    public class SpiralMemoryTest
+    {
+        SpiralMemory spiralMemory;
+
+        [TestInitialize]
+        public void Start()
+        {
+            spiralMemory = new SpiralMemory();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var result = spiralMemory.GetDistance(1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var result = spiralMemory.GetDistance(12);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var result = spiralMemory.GetDistance(23);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var result = spiralMemory.GetDistance(1024);
+            Assert.AreEqual(31, result);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            var result = spiralMemory.GetDistance(361527);
+            Assert.AreEqual(326, result);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day3/SpiralMemory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AdventOfCode2017.Day3
+{
+    public enum Action
+    {
+        IncreaseX, DecreaseX, IncreaseY, DecreaseY
+    }
+
+    public class SpiralMemory
+    {
+        public int GetDistance(int number)
+        {
+            int x = 0;
+            int y = 0;
+            int ring = 0;
+            int ringRemainingElements = 0;
+            int currentElement = 1;
+            var nextAction = Action.IncreaseX;
+
+            while (currentElement != number)
+            {
+                if (ringRemainingElements == 0)
+                {
+                    ring++;
+                    ringRemainingElements = 8 * ring;
+                    nextAction = Action.IncreaseX;
+                }
+
+                switch (nextAction)
+                {
+                    case Action.IncreaseX:
+                        x++;
+                        if (x == ring)
+                        {
+                            nextAction = Action.IncreaseY;
+                        }
+                        break;
+                    case Action.IncreaseY:
+                        y++;
+                        if (y == ring)
+                        {
+                            nextAction = Action.DecreaseX;
+                        }
+                        break;
+                    case Action.DecreaseX:
+                        x--;
+                        if (x == -ring)
+                        {
+                            nextAction = Action.DecreaseY;
+                        }
+                        break;
+                    case Action.DecreaseY:
+                        y--;
+                        if (y == -ring)
+                        {
+                            nextAction = Action.IncreaseX;
+                        }
+                        break;
+                }
+
+                currentElement++;
+                ringRemainingElements--;
+            }
+
+            return Math.Abs(x) + Math.Abs(y);
+        }
+    }
+}
