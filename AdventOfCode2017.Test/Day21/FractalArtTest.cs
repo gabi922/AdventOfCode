ef6fe0f107d2c3ea1a47267c155f0bcf5607b489
Gabi Raducu <gabi.raducu25@gmail.com>
2017-12-21 17:11:04 +0200

    day21: implement helper functions

---
+++ AdventOfCode2017.Test/Day21/FractalArtTest.cs
@@ -0,0 +1,144 @@
+﻿using AdventOfCode2017.Day21;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2017.Test.Day21
+{
+    [TestClass]
+    public class FractalArtTest
+    {
+        FractalArt fractalArt;
+
+        [TestInitialize]
+        public void Start()
+        {
+            fractalArt = new FractalArt();
+        }
+
+        [TestMethod]
+        public void Test_Flip_Horizontally_Odd()
+        {
+            var input = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var expected = new int[,] { { 3, 2, 1 }, { 6, 5, 4 }, { 9, 8, 7 } };
+
+            var actual = fractalArt.FlipHorizontally(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_Flip_Horizontally_Even()
+        {
+            var input = new int[,] { { 1, 2}, { 3, 4 } };
+            var expected = new int[,] { { 2, 1 }, { 4, 3 } };
+
+            var actual = fractalArt.FlipHorizontally(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_Flip_Vertically_Odd()
+        {
+            var input = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var expected = new int[,] { { 7, 8, 9 }, { 4, 5, 6 }, { 1, 2, 3 } };
+
+            var actual = fractalArt.FlipVertically(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_Flip_Vertically_Even()
+        {
+            var input = new int[,] { { 1, 2 }, { 3, 4 } };
+            var expected = new int[,] { { 3, 4 }, { 1, 2 } };
+
+            var actual = fractalArt.FlipVertically(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_Rotate90_Odd()
+        {
+            var input = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var expected = new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } };
+
+            var actual = fractalArt.Rotate90(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_Rotate90_Even()
+        {
+            var input = new int[,] { { 1, 2 }, { 3, 4 } };
+            var expected = new int[,] { { 3, 1 }, { 4, 2 } };
+
+            var actual = fractalArt.Rotate90(input);
+            Assert.IsTrue(fractalArt.AreEqual(expected, actual));
+        }
+
+        [TestMethod]
+        public void Test_ApplyMatchingRule_Simple()
+        {
+            var input = "./Day21/input1.txt";
+            var lines = File.ReadAllLines(input);
+            var rules = fractalArt.GetRules(lines);
+
+            var pattern = new int[,] { { '#', '#' }, { '.', '.' } };
+            var expected = new int[,] { { '#', '.', '.' }, { '#', '#', '#' }, { '#', '.', '.' } };
+
+            var result = fractalArt.ApplyMatchingRule(pattern, rules);
+            Assert.IsTrue(fractalArt.AreEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test_ApplyMatchingRule_FlipVertically()
+        {
+            var input = "./Day21/input1.txt";
+            var lines = File.ReadAllLines(input);
+            var rules = fractalArt.GetRules(lines);
+
+            var pattern = new int[,] { { '.', '.' }, { '#', '#' } };
+            var expected = new int[,] { { '#', '.', '.' }, { '#', '#', '#' }, { '#', '.', '.' } };
+
+            var result = fractalArt.ApplyMatchingRule(pattern, rules);
+            Assert.IsTrue(fractalArt.AreEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test_ApplyMatchingRule_Rotate90()
+        {
+            var input = "./Day21/input1.txt";
+            var lines = File.ReadAllLines(input);
+            var rules = fractalArt.GetRules(lines);
+
+            var pattern = new int[,] { { '#', '.' }, { '#', '.' } };
+            var expected = new int[,] { { '#', '.', '.' }, { '#', '#', '#' }, { '#', '.', '.' } };
+
+            var result = fractalArt.ApplyMatchingRule(pattern, rules);
+            Assert.IsTrue(fractalArt.AreEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test_ApplyMatchingRule_FlipHorizontally()
+        {
+            var input = "./Day21/input1.txt";
+            var lines = File.ReadAllLines(input);
+            var rules = fractalArt.GetRules(lines);
+
+            var pattern = new int[,] { { '#', '#' }, { '.', '#' } };
+            var expected = new int[,] { { '.', '#', '.' }, { '.', '.', '.' }, { '.', '.', '.' } };
+
+            var result = fractalArt.ApplyMatchingRule(pattern, rules);
+            Assert.IsTrue(fractalArt.AreEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test_Generate()
+        {
+            var input = "./Day21/input1.txt";
+            var lines = File.ReadAllLines(input);
+
+            var result = fractalArt.Generate(lines);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day21/FractalArt.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Day21
+{
+    public class FractalArt
+    {
+        public int Generate(string[] lines)
+        {
+            var pattern = new int[,] { { '.', '#', '.' }, { '.', '.', '#' }, { '#', '#', '#' } };
+            var rules = GetRules(lines);
+
+
+
+
+
+
+
+
+
+            return 0;
+        }
+
+        public int[,] Rotate90(int [,] input)
+        {
+            var length = input.GetLength(0);
+            var result = new int[length, length];
+
+            int x = 0;
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = length - 1; j >= 0; j--)
+                {
+                    result[i, x++] = input[j, i];
+                }
+                x = 0;
+            }
+
+            return result;
+        }
+
+        public int[,] FlipHorizontally(int[,] input)
+        {
+            var length = input.GetLength(0);
+            var result = (int[,])input.Clone();
+
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = 0; j < length / 2; j++)
+                {
+                    result[i, j] = input[i, length - j - 1];
+                    result[i, length - j - 1] = input[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public int[,] FlipVertically(int[,] input)
+        {
+            var length = input.GetLength(0);
+            var result = (int[,])input.Clone();
+
+            for (var i = 0; i < length / 2; i++)
+            {
+                for (var j = 0; j < length; j++)
+                {
+                    result[i, j] = input[length - i - 1, j];
+                    result[length - i - 1, j] = input[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public bool AreEqual(int [,] matrix1, int [,] matrix2)
+        {
+            if (matrix1.GetLength(0) != matrix2.GetLength(0))
+            {
+                return false;
+            }
+
+            for (var i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix1.GetLength(0); j++)
+                {
+                    if (matrix1[i, j] != matrix2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public int[,] ApplyMatchingRule(int[,] input, List<Tuple<int[,], int[,]>> rules)
+        {
+            foreach (var rule in rules)
+            {
+                if (AreEqual(input, rule.Item1) || AreEqual(FlipHorizontally(input), rule.Item1) ||
+                    AreEqual(FlipVertically(input), rule.Item1) || AreEqual(Rotate90(input), rule.Item1))
+                {
+                    return rule.Item2;
+                }
+            }
+
+            throw new Exception("No matching rule found");
+        }
+
+        public List<Tuple<int[,], int[,]>> GetRules(string[] lines)
+        {
+            var rules = new List<Tuple<int[,], int[,]>>();
+
+            foreach(var line in lines)
+            {
+                var parts = line.Split(" => ");
+                var inputMatrix = jaggedArrayToMatrix(parts[0].Split('/').Select(l => l.ToCharArray()).ToArray());
+                var outputMatrix = jaggedArrayToMatrix(parts[1].Split('/').Select(l => l.ToCharArray()).ToArray());
+                rules.Add(new Tuple<int[,], int[,]>(inputMatrix, outputMatrix));
+            }
+
+            return rules;
+        }
+
+        private int[,] jaggedArrayToMatrix(char[][] input)
+        {
+            var result = new int[input.Length, input.Length];
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    result[i, j] = input[i][j];
+                }
+            }
+
+            return result;
+        } 
+    }
+}
